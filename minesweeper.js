dae977278305331b0c22d9df03e92c1d36cc4ec7
spectre6000 <spectre6000@gmail.com>
2015-08-17 17:47:14 -0600

    board generation and reset

--- minesweeper.js
+++ minesweeper.js
@@ -4,6 +4,7 @@
   $counter = $container.find('#counter');
   $clock = $container.find('#clock');
   $smiley = $container.find('#smiley');
+  $gameboard = $container.find('#gameboard');
   $setupForm = $container.find('#setupForm');
 
 //House variables
@@ -53,12 +54,24 @@
 
   //Board setup
 
+    function newGame() {
+      cleanBoard();
+      $gameboard.append('<form id="setupForm" action="form_action.asp">' + 
+          'Width: <input type="text" name="width" value="38"><br>' +
+          'Height: <input type="text" name="height" value="38"><br>' +
+          'Hole count: <input type="text" name="holes" value="20"><br>' +
+        '</form>' + 
+        '<button onclick="setup()">Start digging!</button>')
+    }
+
     function setup() {
       var formInput = [];
-      for (var i = 0; i < $setupForm[0].length ;i++) {
+      for (var i = 0; i < $setupForm[0].length; i++) {
         formInput += $setupForm[0].elements[i].value + " ";
       }
       formProcess(formInput);
+      makeBoard();
+      timer();
     }
 
       function formProcess(formInput) {
@@ -68,12 +81,27 @@
         holes = formOutput[2];
       }
 
-      function makeBoard
+      function cleanBoard() {
+        $gameboard.html('');
+      }
+
+      function makeBoard() {
+        cleanBoard();
+        for (var i = 0; i < height; i++) {
+          var h = i + 1;
+          $gameboard.append('<div id="row' + h + '" class="row" ></div>')
+
+          for (var x = 0; x < width; x++) {
+            var w = x + 1;
+            $('#row'+i).append('<span id="row' + h + 'col' + w + '" class="space" ><img src="images/space.jpg"></span>');
+            $gameboard.css({"width": width * 19 + "px"});
+          }
+        }
+      }
 
 $(function(){
-    timer();
   $smiley.click(function() {
     stopTimer();
-    console.log("width: " + width + ", height: " + height + ", holes: " + holes);
+    newGame();
   });
 });
