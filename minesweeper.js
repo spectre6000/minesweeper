b75eae8f2c580dc79c7f2dd783871380b2a98568
spectre6000 <spectre6000@gmail.com>
2015-08-20 17:15:59 -0600

    everything works but winning

--- minesweeper.js
+++ minesweeper.js
@@ -19,6 +19,9 @@
     var holes = 4;
     var holeCoordinates = [];
     var gameOn = true;
+    var remainingHoles = holes;
+    // var remainingCoordinates = [];
+    // var coordinateCountdown = [];
 
 //Movement
 
@@ -58,6 +61,12 @@
       clearInterval( clock );
       seconds = 0;
       minutes = 0;
+    }
+
+  //Counter
+
+    function updateCounter() {
+      remainingHoles < 10 ? $counter.html( '0' + remainingHoles) : $counter.html( remainingHoles )
     }
 
   //Board setup
@@ -124,8 +133,11 @@
           columns = parseInt(formOutput[0]);
           rows = parseInt(formOutput[1]);
           holes = parseInt(formOutput[2]);
+          remainingHoles = holes;
           //new set of holes
           holeCoordinates = [];
+          // remainingCoordinates = [];
+          // coordinateCountdown = [];
         }
 
         function holeRatio() {
@@ -135,18 +147,21 @@
 
       function makeBoard() {
         cleanBoard();
+        updateCounter();
         for (var i = 1; i < parseInt(rows) + 1; i++) {
           //add class for rows (will likely prove unnecessary)
           $gameboard.append('<div id="row' + i + '" class="row" ></div>')
           for (var x = 1; x < parseInt(columns) + 1; x++) {
             //add class for cells by coordinates for targeting
             $('#row'+i).append('<span id="' + i + '-' + x + '" class="space" ><img src="images/space.jpg"></span>');
+            // remainingCoordinates.push([x, i]);
             //give gameboard a defined width for CSS purposes
             $gameboard.css({"width": columns * 19 + "px"});
           }
         }
         //redefine $spaces since they weren't available for initial definitions
         $spaces = $gameboard.find('.space');
+        // remainingCoordinates.sort();
         holeSetup();
       }
 
@@ -167,6 +182,7 @@
               i--;
             } 
           }
+          holeCoordinates.sort();
         }
 
           function randomSpace(axis) {
@@ -185,125 +201,200 @@
       }
     }
 
-  //Game play
-    
-    function play() {
-      $spaces.mousedown(function(event) {
-        if (gameOn) {
-          switch (event.which) {
-            case 1:
-              dig( $(this) );
-              break;
-            case 3:
-              flag( $(this) );
-              break;
-          }
+    function multidimensionalArrayIndex(array, length, pos0, pos1) {
+      //go through a given array (holeCoordinates)
+      for (var i = 0; i < length; i++) {
+        //check both elements of sub-array
+        if ( array[i][0] === pos0 && array[i][1] === pos1 ) {
+          return i;
         }
-      });
+      }
     }
 
-      function dig(space) {
-        //split out coordinates
-        var col = parseInt(space.attr('id').split('-')[0]);
-        var row = parseInt(space.attr('id').split('-')[1]);
-        //make sure it's not already alt marked
-        if ($('#'+col+'-'+row+' img').attr('src')=== 'images/space.jpg') {
-          //check for hole
-          if (multidimensionalArrayContains(holeCoordinates, holeCoordinates.length, col, row)) {
-            showHoles();
-            //change top smiley to digging gif
-            $smiley.attr('src', 'images/digging.gif');
-            gameOn = false;
-          } else {
-            ohMy();
-            //find adjactent holes
-            pokeSand(col, row, []);
-          }
-        }
+  //Game play
+    
+    //Utility functions
+
+      function marker(col, row) {
+        //break out marker
+        return $('#'+col+'-'+row+' img').attr('src');
       }
 
-        function ohMy(){
-          //briefly change top smiley to the OH! face
-          $smiley.attr('src', 'images/ohmy.gif');
-          //then change it back
-          setTimeout( function() {
-            $smiley.attr('src', 'images/smile.gif');
-          }, 200);
-        }
+      function parseSpace(space, index) {
+        return parseInt(space.attr('id').split('-')[index]);
+      }
 
-        function pokeSand(col, row, completedArray) {
-          //define adjacent cells
-          var adjacentCells = populateAdjacentCells(col, row);
-          //counter for clicked space
-          var counter = setCounter(adjacentCells);
-          //set up pass through for recursion
-          var completed = completedArray;
-          completed.push([ col , row ]);
-          //counter > 0 + border limits are the recursive base case
-          pokeSandRecursion(counter, adjacentCells, completed);
-          //indicate with numbered png
-          markHole(col, row, counter);
-        }
+      function changeMarker(col, row, name) {
+        $('#'+col+'-'+row+' img').attr('src', 'images/'+ name);
+      }
 
-          function populateAdjacentCells(col, row){
-            adjacentCells = []
-            adjacentCellOptions = [[col+1, row-1], [col+1, row], [col+1, row+1], [col, row-1], [col, row+1], [col-1, row-1], [col-1, row], [col-1, row+1]];
-            //populate adjacentCells array with viable cell options 
-            for (var i = 0; i < adjacentCellOptions.length; i++) {
-              if (adjacentCellOptions[i][0] > 0 && adjacentCellOptions[i][0] <= columns && adjacentCellOptions[i][1] > 0 && adjacentCellOptions[i][1] <= rows) {
-                adjacentCells.push(adjacentCellOptions[i]);
-              }
+    //Mechanism
+
+    function play() {
+      singleCell();
+      allAdjacent();
+    }
+
+      function singleCell() {
+        $spaces.mousedown(function(event) {
+          if (gameOn) {
+            switch (event.which) {
+              case 1:
+                var col = parseSpace($(this), 0)
+                var row = parseSpace($(this), 1)
+                dig(col, row);
+                break;
+              case 3:
+                flag( $(this) );
+                break;
             }
-            return adjacentCells;
           }
+          // checkForWin();
+        });
+      }
 
-          function setCounter(adjacentCells) {
-            var counter = 0
+        function flag(space) {
+          //split out coordinates
+          var col = parseInt(space.attr('id').split('-')[0]);
+          var row = parseInt(space.attr('id').split('-')[1]);
+          //mark as hole
+          if (marker(col, row) === 'images/space.jpg') {
+            remainingHoles--;
+            updateCounter();
+            changeMarker(col, row, 'mark.jpg');
+          } else if (marker(col, row) === 'images/mark.jpg') {
+            remainingHoles++;
+            updateCounter();
+            changeMarker(col, row, 'question.jpg');
+          } else if (marker(col, row) === 'images/question.jpg') {
+            changeMarker(col, row, 'space.jpg');
+          }
+        }
+
+      function allAdjacent() {
+        $spaces.dblclick(function() {
+          digAllAdjacent( $(this) );
+          // checkForWin();
+        });
+      }
+
+        function digAllAdjacent(space) {
+          //split out coordinates
+          var col = parseSpace(space, 0)
+          var row = parseSpace(space, 1)
+          //make sure it's been played and is a numbered mark
+          if (marker(col, row) !== 'images/space.jpg' && marker(col, row) !== 'images/question.jpg' && marker(col, row) !== 'images/mark.jpg' ) {
+            //define adjacent cells
+            var adjacentCells = populateAdjacentCells(col, row);
             for (var i = 0; i < adjacentCells.length; i++) {
-              //check for match
-              if (multidimensionalArrayContains(holeCoordinates, holeCoordinates.length, adjacentCells[i][0], adjacentCells[i][1])) {
-                //advance counter indicating nuber of adjacent holes
-                counter++;
+              dig(adjacentCells[i][0], adjacentCells[i][1]);
+            }
+          }
+        }
+
+          function dig(col, row) {
+            //make sure it's not already alt marked
+            if (marker(col, row) === 'images/space.jpg') {
+              //check for hole
+              if (multidimensionalArrayContains(holeCoordinates, holeCoordinates.length, col, row)) {
+                showHoles();
+                //change top smiley to digging gif
+                $smiley.attr('src', 'images/digging.gif');
+                gameOn = false;
+              } else {
+                ohMy();
+                //find adjactent holes
+                pokeSand(col, row, []);
               }
             }
-            return counter;
           }
 
-          function pokeSandRecursion(counter, adjacentCells, completed){
-            if (counter === 0) {
-              //if there aren't any adjacent holes
-              for (var i = 0; i < adjacentCells.length; i++) {
-                //run through adjacent cells recursively if they haven't already been done.
-                if (!multidimensionalArrayContains(completed, completed.length, adjacentCells[i][0], adjacentCells[i][1])) {
-                  pokeSand(adjacentCells[i][0], adjacentCells[i][1], completed);
+            // function removeFromRemaining(col,row) {
+            //   console.log('col: ' + col + ', row: ' + row);
+            //   coordinateCountdown = [];
+            //   remainingCoordinates[multidimensionalArrayIndex(remainingCoordinates, remainingCoordinates.length, col, row)] = null;
+            //   for (var i = 0; i < remainingCoordinates.length; i++ ) {
+            //     if (remainingCoordinates[i] !== null) {
+            //       coordinateCountdown.push(remainingCoordinates[i]);
+            //     }
+            //   }
+            //   coordinateCountdown.sort();
+            // }
+
+            function ohMy(){
+              //briefly change top smiley to the OH! face
+              $smiley.attr('src', 'images/ohmy.gif');
+              //then change it back
+              setTimeout( function() {
+                $smiley.attr('src', 'images/smile.gif');
+              }, 200);
+            }
+
+            function pokeSand(col, row, completedArray) {
+              //define adjacent cells
+              var adjacentCells = populateAdjacentCells(col, row);
+              //counter for clicked space
+              var counter = setCounter(adjacentCells);
+              //set up pass through for recursion
+              var completed = completedArray;
+              completed.push([ col , row ]);
+              //counter > 0 + border limits are the recursive base case
+              pokeSandRecursion(counter, adjacentCells, completed);
+              //indicate with numbered png
+              changeMarker(col, row, 'numbers/' + counter + '.png');
+              //pull from remainingCoordinates
+              // removeFromRemaining(col,row);
+            }
+
+              function populateAdjacentCells(col, row){
+                adjacentCells = []
+                adjacentCellOptions = [[col+1, row-1], [col+1, row], [col+1, row+1], [col, row-1], [col, row+1], [col-1, row-1], [col-1, row], [col-1, row+1]];
+                //populate adjacentCells array with viable cell options 
+                for (var i = 0; i < adjacentCellOptions.length; i++) {
+                  if (adjacentCellOptions[i][0] > 0 && adjacentCellOptions[i][0] <= columns && adjacentCellOptions[i][1] > 0 && adjacentCellOptions[i][1] <= rows) {
+                    adjacentCells.push(adjacentCellOptions[i]);
+                  }
+                }
+                return adjacentCells;
+              }
+
+              function setCounter(adjacentCells) {
+                var counter = 0
+                for (var i = 0; i < adjacentCells.length; i++) {
+                  //check for match
+                  if (multidimensionalArrayContains(holeCoordinates, holeCoordinates.length, adjacentCells[i][0], adjacentCells[i][1])) {
+                    //advance counter indicating nuber of adjacent holes
+                    counter++;
+                  }
+                }
+                return counter;
+              }
+
+              function pokeSandRecursion(counter, adjacentCells, completed){
+                if (counter === 0) {
+                  //if there aren't any adjacent holes
+                  for (var i = 0; i < adjacentCells.length; i++) {
+                    //run through adjacent cells recursively if they haven't already been done.
+                    if (!multidimensionalArrayContains(completed, completed.length, adjacentCells[i][0], adjacentCells[i][1])) {
+                      pokeSand(adjacentCells[i][0], adjacentCells[i][1], completed);
+                    }
+                  }
                 }
               }
+
+            function showHoles() {
+              for (var i = 0; i < holeCoordinates.length; i++ ) {
+                $('#' + holeCoordinates[i][0] + '-' + holeCoordinates[i][1]).find('img').attr('src', "images/hole.jpg");
+              }
             }
+
+        function checkForWin() {
+          if (holeCoordinates.length === coordinateCountdown.length && holeCoordinates.equals(coordinateCountdown) ) {
+            $smiley.attr('src', 'images/cool.gif');
+            stopTimer();
           }
-
-          function markHole(col, row, counter) {
-            $('#'+col+'-'+row+' img').attr('src', 'images/numbers/' + counter + '.png');
-          }
-
-      function flag(space) {
-        //split out coordinates
-        var col = parseInt(space.attr('id').split('-')[0]);
-        var row = parseInt(space.attr('id').split('-')[1]);
-        //mark as hole
-        if ($('#'+col+'-'+row+' img').attr('src')=== 'images/space.jpg') {
-          $('#'+col+'-'+row+' img').attr('src', 'images/mark.jpg');
-        } else if ($('#'+col+'-'+row+' img').attr('src')=== 'images/mark.jpg') {
-          $('#'+col+'-'+row+' img').attr('src', 'images/question.jpg');
-        } else if ($('#'+col+'-'+row+' img').attr('src')=== 'images/question.jpg') {
-          $('#'+col+'-'+row+' img').attr('src', 'images/space.jpg');
         }
-      }
 
-      function showHoles() {
-        for (var i = 0; i < holeCoordinates.length; i++ ) {
-          $('#' + holeCoordinates[i][0] + '-' + holeCoordinates[i][1]).find('img').attr('src', "images/hole.jpg");
-        }
-      }
 
     $smiley.click(function() {
       stopTimer();
